00e7de1786246c9fbb83a44bc9b0772826dcb0e2
koios <nabid.akash.official@gmail.com>
2024-07-29 14:27:35 +0600

    added delete expenses

--- src/main/java/com/koios/expense_tracker/controller/ExpenseController.java
+++ src/main/java/com/koios/expense_tracker/controller/ExpenseController.java
@@ -3,24 +3,28 @@ package com.koios.expense_tracker.controller;
 import com.koios.expense_tracker.model.Expense;
 import com.koios.expense_tracker.service.ExpenseService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/expense")
 public class ExpenseController {
     private final ExpenseService expenseService;
+
+    //
     public ExpenseController(ExpenseService expenseService) {
         this.expenseService = expenseService;
     }
+
     @GetMapping("/all")
     public ResponseEntity<?> getAllExpenses() {
         return expenseService.getAllExpenses();
     }
-
     @PostMapping("/add")
     public ResponseEntity<?> addExpense(@RequestBody Expense expense) {
         return expenseService.addExpense(expense);
@@ -29,5 +33,10 @@ public class ExpenseController {
     @GetMapping("/balance")
     public ResponseEntity<?> geBalance() {
         return expenseService.getBalance(1);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteExpense(@RequestParam int id) {
+        return expenseService.deleteExpense(id);
     }
 }
--- src/main/java/com/koios/expense_tracker/service/ExpenseService.java
+++ src/main/java/com/koios/expense_tracker/service/ExpenseService.java
@@ -27,8 +27,9 @@ public class ExpenseService {
 
     @Transactional
     public ResponseEntity<?> addExpense(Expense expense) {
-        User user = userRepo.findById(1).get();
-        if(user.getWallet() == null){
+        Integer id = 1;
+        User user = userRepo.findById(id).get();
+        if (user.getWallet() == null) {
             Wallet wallet = new Wallet();
             user.setWallet(wallet);
             walletRepo.save(wallet);
@@ -37,19 +38,38 @@ public class ExpenseService {
 
         Wallet wallet = user.getWallet();
 
-        if(expense.getPaymentMethod() == PaymentMethod.BANK) {
-            wallet.setBankBalance(wallet.getBankBalance() - expense.getAmount());
+        if (expense.getPaymentMethod() == PaymentMethod.BANK) {
+            int currentBalance = wallet.getBankBalance() - expense.getAmount();
+            if(currentBalance >= 0) {
+                wallet.setBankBalance(currentBalance);
+            }
+            else {
+                return new ResponseEntity<>("Insufficient Balance", HttpStatus.BAD_REQUEST);
+            }
         }
-        else if(expense.getPaymentMethod() == PaymentMethod.BKASH) {
-            wallet.setBkashBalance(wallet.getBkashBalance() - expense.getAmount());
-        } else if (expense.getPaymentMethod() == PaymentMethod.NAGAD) {
-            wallet.setNagadBalance(wallet.getNagadBalance() - expense.getAmount());
+        else if (expense.getPaymentMethod() == PaymentMethod.BKASH) {
+            int currentBalance = wallet.getBkashBalance() - expense.getAmount();
+            if(currentBalance >= 0) {
+                wallet.setBkashBalance(currentBalance);
+            }
+            else {
+                return new ResponseEntity<>("Insufficient Balance", HttpStatus.BAD_REQUEST);
+            }
+        }
+        else if (expense.getPaymentMethod() == PaymentMethod.NAGAD) {
+            int currentBalance = wallet.getNagadBalance() - expense.getAmount();
+            if(currentBalance >= 0) {
+                wallet.setNagadBalance(currentBalance);
+            }
+            else {
+                return new ResponseEntity<>("Insufficient Balance", HttpStatus.BAD_REQUEST);
+            }
         }
         walletRepo.save(wallet);
         user.setWallet(wallet);
         userRepo.save(user);
         expenseRepo.save(expense);
-       return new ResponseEntity<>(userRepo.findAll(), HttpStatus.CREATED);
+        return new ResponseEntity<>(userRepo.findAll(), HttpStatus.CREATED);
     }
 
     public ResponseEntity<?> getAllExpenses() {
@@ -60,9 +80,39 @@ public class ExpenseService {
     }
 
     public ResponseEntity<?> getBalance(Integer id) {
-        if(userRepo.findById(id).isPresent()) {
+        if (userRepo.findById(id).isPresent()) {
             return new ResponseEntity<>(userRepo.findById(id).get().getWallet(), HttpStatus.OK);
         }
         return new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
+    }
+
+    public ResponseEntity<?> deleteExpense(Integer id) {
+        if (userRepo.findById(1).isPresent()) {
+            Wallet wallet = userRepo.findById(1).get().getWallet();
+            if (expenseRepo.findById(id).isPresent()) {
+                String pMethod = expenseRepo.findById(id).get().getPaymentMethod().toString();
+                Integer amount = expenseRepo.findById(id).get().getAmount();
+                expenseRepo.deleteById(id);
+                if(pMethod.equals("BANK")) {
+                    wallet.setBankBalance(wallet.getBankBalance() + amount);
+                }
+                else if(pMethod.equals("NAGAD")) {
+                    wallet.setNagadBalance(wallet.getNagadBalance() + amount);
+                }
+                else if(pMethod.equals("BKASH")) {
+                    wallet.setBkashBalance(wallet.getBkashBalance() + amount);
+                }
+                walletRepo.save(wallet);
+                return new ResponseEntity<>("Successfully deleted", HttpStatus.OK);
+            }
+            return new ResponseEntity<>("Not Found Expense", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>("Not Found User", HttpStatus.NOT_FOUND);
+    }
+
+    @Transactional
+    public ResponseEntity<?> addBalance() {
+        System.out.println("hi");
+        return new ResponseEntity<>("HI", HttpStatus.OK);
     }
 }
